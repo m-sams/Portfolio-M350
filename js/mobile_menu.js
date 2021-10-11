84aa3c2b8c67e01a6d61a37b9fdcfe0fd47fe3d4
m-sams <malachi.samuels@stu.bmcc.cuny.edu>
2021-10-11 17:27:36 -0400

    // layout changes made to home and classes pgs. // functional mobile menu added for small screens. // desktop layouts done for home and classes pgs. // replaced footer links w. images

---
+++ js/mobile_menu.js
@@ -0,0 +1,6 @@
+function side_nav_open(){
+    document.getElementById("side-nav").style.display="block";
+}
+function side_nav_close(){
+    document.getElementById("side-nav").style.display="none";
+}
